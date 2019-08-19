173884dfa09f01e01e60c8f2de043d0fd622ed70
juser0719 <sinsa0719@naver.com>
2019-08-19 18:01:39 +0900

    jeon

---
+++ src/main/java/com/example/spring/controller/membercontroller.java
@@ -0,0 +1,10 @@
+package com.example.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class membercontroller {
+    @RequestMapping(value = "/member/list")
+    public void memberAction(){}
+}
