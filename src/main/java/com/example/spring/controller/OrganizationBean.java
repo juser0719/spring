2a6fb250e3c98b4e7239d69469b4ff9b95096514
juser0719 <sinsa0719@naver.com>
2019-08-20 16:23:46 +0900

    jeon

---
+++ src/main/java/com/example/spring/controller/OrganizationBean.java
@@ -0,0 +1,10 @@
+package com.example.spring.controller;
+
+public class OrganizationBean 
+{
+    private String id;
+
+    public String getId(){ return id;}
+    public void setId(String id) {this.id = id;}
+
+}
---
+++ src/main/java/com/example/spring/controller/OrganizationController.java
@@ -0,0 +1,29 @@
+package com.example.spring.controller;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+
+public class OrganizationController
+{
+   @RequestMapping(value = "/input", method = RequestMethod.GET)
+   public void some(){}
+   @RequestMapping(value = "/read", method = RequestMethod.GET)
+    public ModelAndView read(@RequestParam Map<String , Object> paramMap,ModelAndView modelandView) 
+    {
+    
+    String viewName = "/read";
+    modelandView.setViewName(viewName);
+    modelandView.addObject("Id", paramMap);
+
+    return modelandView;
+    }
+
+
+}
